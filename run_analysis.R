36d4b2fa9dd1bdbe74a143bbd6180643e3b176a4
santule <sanjana.tule@gmail.com>
2016-07-30 21:31:17 +1000

    version 1

--- run_analysis.R
+++ run_analysis.R
@@ -1,34 +1,31 @@
-# set working directory
-store_wd <- getwd()
-setwd("C:/Users/vivek verma/Desktop/Data Science/Data/UCI HAR Dataset")
 
 # Extract Test Data
-test_data <- read.table("./test/X_test.txt",stringsAsFactors = FALSE)
+test_data <- read.table("./UCI HAR Dataset/test/X_test.txt",stringsAsFactors = FALSE)
 
 # Find mean and sd of all rows
 mean <- apply(test_data,1,mean,na.rm= TRUE)
 standard_deviation <- apply(test_data,1,sd,na.rm= TRUE)
 
 #Extact subject
-subject_test <- read.fwf("./test/subject_test.txt",col.names = "subject",widths=2)
+subject_test <- read.fwf("./UCI HAR Dataset/test/subject_test.txt",col.names = "subject",widths=2)
 
 #Extact activity code
-activitycode_test <- read.fwf("./test/y_test.txt",col.names = "activitycode",widths=1)
+activitycode_test <- read.fwf("./UCI HAR Dataset/test/y_test.txt",col.names = "activitycode",widths=1)
 
 #Combine all together for test data
 test_data_final<- cbind(subject_test,activitycode_test,mean,standard_deviation)
 
 # Extract Train Data
-train_data <- read.table("./train/X_train.txt",stringsAsFactors = FALSE)
+train_data <- read.table("./UCI HAR Dataset/train/X_train.txt",stringsAsFactors = FALSE)
 # Find mean and sd of all rows
 mean <- apply(train_data,1,mean,na.rm= TRUE)
 standard_deviation <- apply(train_data,1,sd,na.rm= TRUE)
 
 #Extact subject
-subject_train <- read.fwf("./train/subject_train.txt",col.names = "subject",widths=2)
+subject_train <- read.fwf("./UCI HAR Dataset/train/subject_train.txt",col.names = "subject",widths=2)
 
 #Extact activity code
-activitycode_train <- read.fwf("./train/y_train.txt",col.names = "activitycode",widths=1)
+activitycode_train <- read.fwf("./UCI HAR Dataset/train/y_train.txt",col.names = "activitycode",widths=1)
 
 #Combine all together for train data
 train_data_final<- cbind(subject_train,activitycode_train,mean,standard_deviation)
@@ -37,7 +34,7 @@ train_data_final<- cbind(subject_train,activitycode_train,mean,standard_deviatio
 combined_data <- rbind(test_data_final,train_data_final)
 
 # Extract Activity Label
-activity_label <- read.table("activity_labels.txt",col.names = c("activitycode","activity"),stringsAsFactors = FALSE)
+activity_label <- read.table("./UCI HAR Dataset/activity_labels.txt",col.names = c("activitycode","activity"),stringsAsFactors = FALSE)
 
 # Activity label merge with combined data
 combined_data <- merge(combined_data,activity_label)
@@ -56,7 +53,7 @@ tidy_data <- merge(tidy_data,activity_label)
 # reorder columns in tidy data set
 tidy_data <- tidy_data[c(1,5,2,3,4)]
 
-setwd(store_wd) 
+
 
 # write data to the .csv file.
 write.table(combined_data,"combined_data.txt",row.name= FALSE)
